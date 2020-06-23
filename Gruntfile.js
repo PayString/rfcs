82d111dadee6cc85ee74d8268335fd12ab7f4333
Hans Bergren <hansbergren@gmail.com>
2020-06-23 11:12:33 -0400

    wip

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,42 @@
 module.exports = function(grunt) {
+  // Add new RFC proposals to this list:
+  const rfcs = [
+    "src/spec/payid-uri.md",
+    "src/spec/payid-discovery.md",
+    "src/spec/payid-protocol.md",
+    "src/spec/verifiable-payid-protocol.md"
+  ]
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      scripts: {
-        files: ['src/spec/payid-uri.md', 'src/spec/payid-discovery.md', 'src/spec/payid-protocol.md', 'src/spec/verifiable-payid-protocol.md'],
-        tasks: ['kramdown_rfc2629'],
-        options: {
-          spawn: false,
-        },
-      },
-    },
+    pkg: grunt.file.readJSON("package.json"),
+
+    // The command that generates specs
     kramdown_rfc2629: {
       options: {
-        outputs: ['text', 'html'],
-        outputDir: 'dist/spec',
+        outputs: ["text", "html"],
+        outputDir: "dist/spec",
         removeXML: false
       },
       your_target: {
-        src: ['src/spec/payid-uri.md', 'src/spec/payid-discovery.md', 'src/spec/payid-protocol.md', 'src/spec/verifiable-payid-protocol.md']
-      },
+        src: rfcs,
+      }
     },
+
+    // The "watch" command which will watch specs for file changes,
+    // and automatically regenerate the RFC outputs.
+    watch: {
+      scripts: {
+        files: rfcs,
+        tasks: ["kramdown_rfc2629"],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-kramdown-rfc2629');
-
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-kramdown-rfc2629");
 };
